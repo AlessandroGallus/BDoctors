6bf4f64e2178e1afd28a338632b0f300f8e70cce
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-22 10:43:52 +0200

    prova_rotta_test_vue

--- public/js/app.js
+++ public/js/app.js
@@ -2008,6 +2008,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'Header'
 });
@@ -3307,7 +3308,9 @@ var render = function() {
               1
             ),
             _vm._v(" "),
-            _vm._m(0)
+            _vm._m(0),
+            _vm._v(" "),
+            _vm._m(1)
           ])
         ])
       ]
@@ -3321,6 +3324,14 @@ var staticRenderFns = [
     var _c = _vm._self._c || _h
     return _c("li", { staticClass: "d-inline" }, [
       _c("a", { attrs: { href: "/register" } }, [_vm._v("Register")])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("li", { staticClass: "d-inline" }, [
+      _c("a", { attrs: { href: "/register" } }, [_vm._v("test")])
     ])
   }
 ]
