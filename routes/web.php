4296aa992da2f8117db76cd9f2d4ba89b60c9ad0
AlessandroGallus <algal543@gmail.com>
2021-07-28 16:02:21 +0200

    Merge branch 'master' of https://github.com/AlessandroGallus/prova-master

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,5 +32,56 @@ Route::namespace('Admin')
 Route::get('/dashboard', 'HomeController@index')->name('home');
 Route::get('/dashboard/messages', 'HomeController@messages')->name('messages');
 Route::get('/dashboard/reviews', 'HomeController@reviews')->name('messages');
+
+
+Route::get('/dashboard/payment',  function(){
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.enviroment'),
+        'merchantId' => config('services.braintree.merchantId'),
+        'publicKey' => config('services.braintree.publicKey'),
+        'privateKey' => config('services.braintree.privateKey')
+    ]);
+    $token = $gateway->ClientToken()->generate();
+    return view('Admin.payment', [
+        'token'=>$token
+    ]);
+})->name('payment');
+
+Route::post('/checkout', function (Request $request) {
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.enviroment'),
+        'merchantId' => config('services.braintree.merchantId'),
+        'publicKey' => config('services.braintree.publicKey'),
+        'privateKey' => config('services.braintree.privateKey')
+    ]);
+    $amount = $request->amount;
+    $nonce = $request->payment_method_nonce;
+
+    $result = $gateway->transaction()->sale([
+        'amount' => $amount,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+            'submitForSettlement' => true
+        ]
+    ]);
+
+    if ($result->success) {
+        $transaction = $result->transaction;
+        /* header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id); */
+
+        return redirect()->back()->with('success_message','Transaction Successful. The Id is    ' .$transaction->id);
+    } else {
+        $errorString = "";
+
+        foreach ($result->errors->deepAll() as $error) {
+            $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+        }
+
+        return redirect()->back()->withErrors('Errore:' .$result->message);
+    }
+});
+
+
+
 Route::delete('/dashboard/{user}','Admin\UserController@destroy')->middleware('auth')->name('user.destroy');
 Route::get('{any?}', 'PageController@index')->where('any','.*');
