a8ecabe673382247a2065eb1ebb1088bfe1ad3d9
bigoroberto <bigoroberto@gmail.com>
2021-08-02 15:26:47 +0200

    front

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,34 +1,61 @@
 <template>
-      <footer>
-        <div class="footer bg_dark_blue padding_top_60 padding_bottom_60">
-          <div class="container">
-            <div class="row">
-              <div class="col-xs-12 col-md-4 white_opacity_color">
-                <p class="margin_top_10">
-                <router-link :to="{name:'home'}"><img src="../../../public/img/logo1.png" alt="Logo"></router-link>
-                  &copy;  bDoctors. All rights reserved.<br>
-                  <!-- <a href="#" class="grey_white_link">Proudly made in NY</a> -->
-                </p>
-                <ul class="social-icon">
-                  <li>
-                    <a target="_blank" href="https://twitter.com/idoctors_it" class="grey_white_link"><i class="fab fa-twitter"></i></a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://www.facebook.com/iDoctors.it/" class="grey_white_link"><i class="fab fa-facebook-square"></i></a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://www.instagram.com/idoctors.it/?hl=it" class="grey_white_link"><i class="fab fa-instagram"></i></a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://www.youtube.com/watch?v=UzFbEJKoRWc" class="grey_white_link"><i class="fab fa-youtube"></i></a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://www.linkedin.com/company/idoctors-it/about/" class="grey_white_link"><i class="fab fa-linkedin"></i></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-xs-12 col-sm-6 col-md-2">
-                <h3 class="white_color">FAQ</h3>
+  <footer>
+    <div class="footer bg_dark_blue padding_top_60 padding_bottom_60">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-4 white_opacity_color">
+            <p class="margin_top_10">
+              <router-link :to="{ name: 'home' }"
+                ><img src="../../../public/img/logo1.png" alt="Logo"
+              /></router-link>
+              &copy; bDoctors. All rights reserved.<br />
+              <!-- <a href="#" class="grey_white_link">Proudly made in NY</a> -->
+            </p>
+            <ul class="social-icon">
+              <li>
+                <a
+                  target="_blank"
+                  href="https://twitter.com/idoctors_it"
+                  class="grey_white_link"
+                  ><i class="fab fa-twitter"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/iDoctors.it/"
+                  class="grey_white_link"
+                  ><i class="fab fa-facebook-square"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.instagram.com/idoctors.it/?hl=it"
+                  class="grey_white_link"
+                  ><i class="fab fa-instagram"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.youtube.com/watch?v=UzFbEJKoRWc"
+                  class="grey_white_link"
+                  ><i class="fab fa-youtube"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/company/idoctors-it/about/"
+                  class="grey_white_link"
+                  ><i class="fab fa-linkedin"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-2">
+            <h3 class="white_color">FAQ</h3>
             <!--     <ul class="vertical_menu">
                   <li>
                     <a href="#" class="grey_white_link">About</a>
@@ -61,24 +88,44 @@
                     <a href="#" class="grey_white_link">Legal &amp; Security</a>
                   </li>
                 </ul> -->
-              </div>
-              <div class="col-xs-12 col-sm-6 col-md-2">
-                <h3 class="white_color">Contatti</h3>
-               <ul class="vertical_menu">
-                  <li>
-                    <a target="_blank" href="https://github.com/merlino79" class="grey_white_link">Alessandro C.</a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://github.com/AlessandroGallus" class="grey_white_link">Alessandro G.</a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://github.com/AjayNaruka" class="grey_white_link">Ajay N.</a>
-                  </li>
-                  <li>
-                    <a target="_blank" href="https://github.com/bigoroberto" class="grey_white_link">Roberto B.</a>
-                  </li>
-               </ul>
-               <!--
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-2">
+            <h3 class="white_color">Contatti</h3>
+            <ul class="vertical_menu">
+              <li>
+                <a
+                  target="_blank"
+                  href="https://github.com/merlino79"
+                  class="grey_white_link"
+                  >Alessandro C.</a
+                >
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://github.com/AlessandroGallus"
+                  class="grey_white_link"
+                  >Alessandro G.</a
+                >
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://github.com/AjayNaruka"
+                  class="grey_white_link"
+                  >Ajay N.</a
+                >
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://github.com/bigoroberto"
+                  class="grey_white_link"
+                  >Roberto B.</a
+                >
+              </li>
+            </ul>
+            <!--
                   <li>
                     <a href="#" class="grey_white_link">Block Storage</a>
                   </li>
@@ -101,7 +148,7 @@
                     <a href="#" class="grey_white_link">Release Notes</a>
                   </li>
                 </ul> -->
-              </div>
+          </div>
           <!--     <div class="col-xs-12 col-sm-6 col-md-2">
                 <h3 class="white_color">Community</h3>
                 <ul class="vertical_menu">
@@ -137,9 +184,9 @@
                   </li>
                 </ul>
               </div> -->
-              <div class="col-xs-12 col-sm-6 col-md-2">
-                <h3 class="white_color">Assistenza</h3>
-               <!--  <ul class="vertical_menu">
+          <div class="col-xs-12 col-sm-6 col-md-2">
+            <h3 class="white_color">Assistenza</h3>
+            <!--  <ul class="vertical_menu">
                   <li>
                     <a href="#" class="grey_white_link">Contact support</a>
                   </li>
@@ -150,60 +197,61 @@
                     <a href="#" class="grey_white_link">Network Status</a>
                   </li>
                 </ul> -->
-              </div>
-            </div>
           </div>
         </div>
-      </footer>
-      <!-- /footer -->
+      </div>
+    </div>
+  </footer>
+  <!-- /footer -->
 </template>
 
 <script>
 export default {
-    name:"Footer",
-}
+  name: "Footer",
+};
 </script>
 
 <style lang="scss" scoped>
-footer{
-    background-image: linear-gradient( to right , #386db3, #56a7da);
-    justify-content: space-between
+footer {
+  background-image: linear-gradient(to right, #1e6650, #63987a);
+  justify-content: space-between;
 }
-ul.vertical_menu{
+ul.vertical_menu {
   list-style-type: none;
   margin: 0;
   padding: 0;
 }
-ul.vertical_menu li{
+ul.vertical_menu li {
   margin-bottom: 10px;
 }
-ul.social-icon{
+ul.social-icon {
   color: #fff;
   list-style-type: none;
   margin: 0;
   padding: 0;
-  &:hover{
-        color: #ff6600;
-    }
+  &:hover {
+    color: #ff6600;
+  }
 }
-ul.social-icon li{
+ul.social-icon li {
   display: inline-block;
   margin-right: 10px;
   font-size: 1.5em;
 }
 
-a{
-    color: #fff;
-    &:hover{
-        color: #ff6600;
-    }
+a {
+  color: #fff;
+  &:hover {
+    color: #ff6600;
+  }
 }
 
-h3,p{
-    color: #fff;
-    text-align: center;
+h3,
+p {
+  color: #fff;
+  text-align: center;
 }
-img{
-    width: 70px;
+img {
+  width: 70px;
 }
 </style>
--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,101 +1,109 @@
 <template>
   <header class="mb-3">
-    <div class="container ">
-
-        <nav class="d-flex justify-content-between align-items-center">
-      <div class="left home">
-         <router-link :to="{name:'home'}"><img src="../../../public/img/logo1.png" alt="Logo"></router-link>
-         
-     </div>
-      <div class='right'>
-      
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-        <ul class="list-style-none list">
-         <router-link :to="{name:'doctorlist'}"> <li class="d-inline">Dottori</li> </router-link>
-         <router-link :to="{name:'advancedSearch'}"><li class="d-inline">Ricerca</li> </router-link>
-         <a href="/register"><li class="d-inline">Registrati</li></a>
-
-        </ul>
-      </div>
-    </nav>
+    <div class="container">
+      <nav class="d-flex justify-content-between align-items-center">
+        <div class="left home">
+          <router-link :to="{ name: 'home' }"
+            ><img src="../../../public/img/logo1.png" alt="Logo"
+          /></router-link>
+        </div>
+        <div class="right">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul class="list-style-none list">
+            <router-link :to="{ name: 'doctorlist' }">
+              <li class="d-inline">Dottori</li>
+            </router-link>
+            <router-link :to="{ name: 'advancedSearch' }"
+              ><li class="d-inline">Ricerca</li>
+            </router-link>
+            <a href="/register"><li class="d-inline">Registrati</li></a>
+          </ul>
+        </div>
+      </nav>
     </div>
-
   </header>
 </template>
 
 <script>
 export default {
-    name:'Header'
-}
+  name: "Header",
+};
 </script>
 
 <style lang='scss' scoped>
+header {
+  display: flex;
+  align-items: center;
+  color: white;
+  height: 60px;
+  line-height: 60px;
+  /* background-color: #386db3; */
+  background-image: linear-gradient(to right, #1e6650, #63987a);
+  nav {
+    height: inherit;
 
-  header{
-    display: flex;
-    align-items: center;
-    color: white;
-    height: 60px;
-    line-height: 60px;
-    /* background-color: #386db3; */
-    background-image: linear-gradient( to right , #386db3, #56a7da);
-    nav{
-      height: inherit;
+    img {
+      width: 50px;
+    }
+    .home {
+      font-size: 2rem;
+    }
 
-      img{
-          width: 50px;
-      }
-      .home{
-          font-size: 2rem;
-      }
+    a {
+      color: #fff;
+      text-decoration: none;
+    }
 
-      a{
-          color: #fff;
-          text-decoration: none;
-      }
-
-      .right{
-          display: flex;
-          align-items: center;
-          .list{
-              margin-bottom: 0;
-               font-size:22px;
-               a{
-                   color: white;
-                   &:hover{
-                       color: #ff6600;
-                   }
-               }
-               li::after{
-                 display: block;
-                 position: absolute;
-                 content: '';
-                 top: 50px;
-                 left: 0;
-                 height: 1px;
-                 width: 0%;
-                 background-color: white;
-                 -webkit-transition: width .3s;
-               }
-               li{
-                 font-size: 15px;
-                 position: relative;
-                  /*  margin: 0 10px; */
-                  height: 100%;
-                  padding: 25px;
-                  &:hover{
-                    /* background-color: #043d6b;
-                    transition: .5s; */
-                    &::after{
-                      width: 100%;
-                      transition:ease-in-out .3s;
-                    }
-                  }
-               }
+    .right {
+      display: flex;
+      align-items: center;
+      .list {
+        margin-bottom: 0;
+        font-size: 22px;
+        a {
+          color: white;
+          &:hover {
+            color: #ff6600;
           }
+        }
+        li::after {
+          display: block;
+          position: absolute;
+          content: "";
+          top: 50px;
+          left: 0;
+          height: 1px;
+          width: 0%;
+          background-color: white;
+          -webkit-transition: width 0.3s;
+        }
+        li {
+          font-size: 15px;
+          position: relative;
+          /*  margin: 0 10px; */
+          height: 100%;
+          padding: 25px;
+          &:hover {
+            /* background-color: #043d6b;
+                    transition: .5s; */
+            &::after {
+              width: 100%;
+              transition: ease-in-out 0.3s;
+            }
+          }
+        }
       }
     }
   }
+}
 </style>
