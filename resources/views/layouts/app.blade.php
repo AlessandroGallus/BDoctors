d5b681a40d65a80e8539b6f4ae8accd024096412
AjayNaruka <naruka.ajay1997@gmail.com>
2021-08-04 11:16:22 +0200

    dashboard front end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,7 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="/dashboard">Dashboard</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
